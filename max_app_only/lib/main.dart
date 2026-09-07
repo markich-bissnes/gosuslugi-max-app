875801965df1c2d9f01b756ee9771a72373462f5
markich-bissnes <markys14837@gmail.com>
2026-09-07 21:46:24 +0300

    fix const error in max_app main.dart

--- max_app_only/lib/main.dart
+++ max_app_only/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'MAX',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
-      home: WebScreen(),
+      home: const WebScreen(),
     );
   }
 }
